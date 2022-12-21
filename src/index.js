d5e900f410f2bf5c1503a7c4ba254c32629e7e29
root <root@90692.web.hosting-russia.ru>
2022-12-21 18:00:28 +0300

    PIZDEC 5, KAK JE YA ZAEBALSYA, PIZDEEEEC

--- src/index.js
+++ src/index.js
@@ -4,32 +4,11 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import ListPage from './pages/list/list';
 
-const tasks = {
-  1: {
-    description: "test",
-    taskname: "test",
-    status: "test",
-    image: "1.jpg"
-  },
-  2: {
-    description: "test",
-    taskname: "test",
-    status: "test",
-    image: "2.jpeg"
-  },
-  3: {
-    description: "test",
-    taskname: "test",
-    status: "test",
-    image: "3.jpg"
-  },
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ListPage tasks={Object.keys(tasks).map(a => ({"id": a, ...tasks[a]}))}/>
+      <ListPage />
     </Provider>
   </React.StrictMode>
 );
