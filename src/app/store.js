dc9fd608325ec4f9ffbbd8d2af42426357f6082b
Alexey Slokva <Alesha72003@ya.ru>
2022-12-22 00:32:07 +0300

    PIZDEC 6, POCHTI ZAKONCHIL S FRONTOM

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 import listReducer from '../features/list/listSlice';
+import filterReducer from "../features/list/filterSlice";
 
 export const store = configureStore({
   reducer: {
     // counter: counterReducer,
-    list: listReducer
+    list: listReducer,
+    filter: filterReducer
   },
 });
