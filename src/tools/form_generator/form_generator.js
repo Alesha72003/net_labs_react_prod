4b0f7cfc3d824a1804ad7b42b963fc633483a20c
Alexey Slokva <Alesha72003@ya.ru>
2022-12-30 00:50:19 +0300

    УРААА! ЕСТЬ ШАБЛОНИЗАЦИЯ ФОРМОЧЕК

---
+++ src/tools/form_generator/form_generator.js
@@ -0,0 +1,43 @@
+import { Form } from "react-bootstrap";
+import { useState } from "react";
+
+export function Group(config) {
+  return (
+    <Form.Group controlId={config.name} name={config.name}>
+      <Form.Label>{config.label}</Form.Label>
+      {config.item}
+    </Form.Group>
+  );
+}
+
+export function Checkbox(config) {
+  const [checked, setChecked] = useState(true);
+  return (
+    <Form.Check
+      inline
+      label={config.label}
+      name={config.name}
+      type="checkbox"
+      checked={checked}
+      onChange={() => setChecked(!checked)}
+    />
+  );
+}
+
+export function CustomForm({children, onSubmitData}) {
+
+  function onSubmitHandle(e) {
+    e.preventDefault();
+    const items = Object.keys(e.target.elements).map(el => e.target.elements[el].name || e.target.elements[el].id).filter(el => el)
+    console.log(e, items);
+    const formData = items.reduce((a, v) => ({...a, [v]: e.target[v].type === "checkbox" ? e.target[v].checked : e.target[v].value}), {});
+    return onSubmitData(formData);
+  }
+
+  return (
+    <Form onSubmit={onSubmitHandle}>
+      {children}
+    </Form>
+  )
+
+}
